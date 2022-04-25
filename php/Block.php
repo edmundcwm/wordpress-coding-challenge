880f63ec334eb74307d9c7c6e319cf7bed856e9e
Edmund Chan <edmundcwm@gmail.com>
2022-04-25 21:28:30 +0800

    Fix escaping and i18n

--- php/Block.php
+++ php/Block.php
@@ -69,8 +69,8 @@ class Block {
 		ob_start();
 
 		?>
-		<div class="<?php echo $class_name; ?>">
-			<h2>Post Counts</h2>
+		<div class="<?php echo esc_attr( $class_name ); ?>">
+			<h2><?php echo esc_html__( 'Post Counts', 'site-counts' ); ?></h2>
 			<ul>
 		<?php
 		foreach ( $post_types as $post_type_slug ) :
@@ -86,12 +86,23 @@ class Block {
 
 			?>
 				<li>
-			<?php
-			echo 'There are ' . $post_count . ' ' . $post_type_object->labels->name . '.';
-			?>
+					<?php
+					// translators: %1$d: post count, %2$s: post type name.
+					printf( esc_html__( 'There are %1$d %2$s.', 'site-counts' ), esc_html( $post_count ), esc_html( $post_type_object->labels->name ) );
+					?>
 				</li>
 		<?php endforeach; ?>
-			</ul><p><?php echo 'The current post ID is ' . $_GET['post_id'] . '.'; ?></p>
+			</ul>
+			<p>
+				<?php
+				$post_id = isset( $_GET['post_id'] ) ? filter_input( INPUT_GET, $_GET['post_id'], FILTER_SANITIZE_NUMBER_INT ) : false; //phpcs:ignore
+				/* translators: %d: post ID */
+				$content = false !== $post_id ? sprintf( __( 'The current post ID is %d', 'site-counts' ), $post_id ) : __( 'Invalid post ID', 'site-counts' );
+
+				echo esc_html( $content );
+				?>
+			</p>
+
 
 		<?php
 		$query = new WP_Query(
@@ -116,13 +127,13 @@ class Block {
 
 		if ( $query->have_posts() ) :
 			?>
-				<h2>5 posts with the tag of foo and the category of baz</h2>
+				<h2><?php echo esc_html__( '5 posts with the tag of foo and the category of baz', 'site-counts' ); ?></h2>
 				<ul>
 			<?php
 
 			foreach ( array_slice( $query->posts, 0, 5 ) as $post ) :
 				?>
-					<li><?php echo $post->post_title; ?></li>
+					<li><?php echo wp_kses_post( $post->post_title ); ?></li>
 				<?php
 			endforeach;
 		endif;
