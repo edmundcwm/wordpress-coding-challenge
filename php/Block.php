7cd6f601f96f2b4d1dcc11a3f1c545b2eb61a9bf
Edmund Chan <edmundcwm@gmail.com>
2022-04-25 21:34:25 +0800

    Fix indentation

--- php/Block.php
+++ php/Block.php
@@ -65,33 +65,33 @@ class Block {
 	 */
 	public function render_callback( $attributes, $content, $block ) {
 		$post_types = get_post_types( [ 'public' => true ] );
-		$class_name = $attributes['className'];
+		$class_name = ! empty( $attributes['className'] ) ? $attributes['className'] : '';
 		ob_start();
 
 		?>
 		<div class="<?php echo esc_attr( $class_name ); ?>">
 			<h2><?php echo esc_html__( 'Post Counts', 'site-counts' ); ?></h2>
 			<ul>
-		<?php
-		foreach ( $post_types as $post_type_slug ) :
-			$post_type_object = get_post_type_object( $post_type_slug );
-			$post_count       = count(
-				get_posts(
-					[
-						'post_type'      => $post_type_slug,
-						'posts_per_page' => -1,
-					]
-				)
-			);
+				<?php
+				foreach ( $post_types as $post_type_slug ) :
+					$post_type_object = get_post_type_object( $post_type_slug );
+					$post_count       = count(
+						get_posts(
+							[
+								'post_type'      => $post_type_slug,
+								'posts_per_page' => -1,
+							]
+						)
+					);
 
-			?>
-				<li>
-					<?php
-					// translators: %1$d: post count, %2$s: post type name.
-					printf( esc_html__( 'There are %1$d %2$s.', 'site-counts' ), esc_html( $post_count ), esc_html( $post_type_object->labels->name ) );
 					?>
-				</li>
-		<?php endforeach; ?>
+					<li>
+						<?php
+						// translators: %1$d: post count, %2$s: post type name.
+						printf( esc_html__( 'There are %1$d %2$s.', 'site-counts' ), esc_html( $post_count ), esc_html( $post_type_object->labels->name ) );
+						?>
+					</li>
+				<?php endforeach; ?>
 			</ul>
 			<p>
 				<?php
@@ -102,43 +102,36 @@ class Block {
 				echo esc_html( $content );
 				?>
 			</p>
-
-
-		<?php
-		$query = new WP_Query(
-			[
-				'post_type'     => [ 'post', 'page' ],
-				'post_status'   => 'any',
-				'date_query'    => [
-					[
-						'hour'    => 9,
-						'compare' => '>=',
+			<?php
+			$query = new WP_Query(
+				[
+					'post_type'     => [ 'post', 'page' ],
+					'post_status'   => 'any',
+					'date_query'    => [
+						[
+							'hour'    => 9,
+							'compare' => '>=',
+						],
+						[
+							'hour'    => 17,
+							'compare' => '<=',
+						],
 					],
-					[
-						'hour'    => 17,
-						'compare' => '<=',
-					],
-				],
-				'tag'           => 'foo',
-				'category_name' => 'baz',
-				'post__not_in'  => [ get_the_ID() ],
-			]
-		);
+					'tag'           => 'foo',
+					'category_name' => 'baz',
+					'post__not_in'  => [ get_the_ID() ],
+				]
+			);
 
-		if ( $query->have_posts() ) :
-			?>
+			if ( $query->have_posts() ) :
+				?>
 				<h2><?php echo esc_html__( '5 posts with the tag of foo and the category of baz', 'site-counts' ); ?></h2>
 				<ul>
-			<?php
-
-			foreach ( array_slice( $query->posts, 0, 5 ) as $post ) :
-				?>
-					<li><?php echo wp_kses_post( $post->post_title ); ?></li>
-				<?php
-			endforeach;
-		endif;
-		?>
-			</ul>
+					<?php foreach ( array_slice( $query->posts, 0, 5 ) as $post ) : ?>
+						<li><?php echo wp_kses_post( $post->post_title ); ?></li>
+					<?php endforeach; ?>
+				</ul>
+			<?php endif; ?>
 		</div>
 		<?php
 
